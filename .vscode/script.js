e91d92412766b2570e6e5336258b2d0699216f98
Evandro Teixeira <tevandro.075@gmail.com>
2024-04-22 17:43:42 -0300

    adjust container width

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,17 +1,15 @@
-function toggleMode () {
+function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
- 
-  const img = document.querySelector("#profile")
 
-  if(html.classList.contains('light')){
-
-    img.setAttribute("src", "./assets/scorpion.jpg.jpg")} else {
-
+  //pegar a atag amg
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se estiver light mode , adicionarr a imagem light
+    img.setAttribute("src", "./assets/scorpion.jpg.jpg")
+  } else {
+    //se estiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/black.panther.jpg")
   }
-
-
-
-
 }
